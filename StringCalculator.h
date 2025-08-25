b7c7abcaf49fdf4353ee8e075104eba8b51da66d
Suman Kundu <kundu7878@gmail.com>
2025-08-25 17:51:52 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,7 +1,11 @@
-#pragma once
+#ifndef STRINGCALCULATOR_H_
+#define STRINGCALCULATOR_H_
+
 #include <string>
 
 class StringCalculator {
  public:
     int Add(const std::string& numbers);
 };
+
+#endif  // STRINGCALCULATOR_H_

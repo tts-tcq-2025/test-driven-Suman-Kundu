1223879e3a0894eea19055f37ca02b8bd70ed2b0
Suman Kundu <kundu7878@gmail.com>
2025-08-25 16:58:53 +0530

    Update DelimiterParser.h

--- DelimiterParser.h
+++ DelimiterParser.h
@@ -6,4 +6,7 @@ public:
     static bool hasCustomDelimiter(const std::string& numbers);
     static std::string extractCustomDelimiter(const std::string& numbers);
     static std::string extractNumbers(const std::string& numbers);
+
+private:
+    static std::string parseMultiCharDelimiter(const std::string& delimiterPart);
 };

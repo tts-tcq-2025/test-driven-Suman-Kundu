f6bdb1f216755b8e216354f05e0d947ed9a103c3
Suman Kundu <kundu7878@gmail.com>
2025-08-25 16:59:31 +0530

    Update NumberProcessor.h

--- NumberProcessor.h
+++ NumberProcessor.h
@@ -10,4 +10,7 @@ public:
 
 private:
     static std::vector<std::string> split(const std::string& text, const std::string& delimiters);
+    static void addToken(std::vector<std::string>& tokens, const std::string& text, size_t start, size_t end);
+    static std::vector<int> findNegatives(const std::vector<int>& numbers);
+    static void throwNegativeException(const std::vector<int>& negatives);
 };

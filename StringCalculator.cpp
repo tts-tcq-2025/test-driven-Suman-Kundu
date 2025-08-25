7c48082fbe28cae48bf2c5675004139e18ae9d4a
Suman Kundu <kundu7878@gmail.com>
2025-08-25 17:39:22 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,4 +1,5 @@
 #include "StringCalculator.h"
+#include <string>
 #include "DelimiterParser.h"
 #include "NumberProcessor.h"
 
@@ -8,17 +9,17 @@ int StringCalculator::Add(const std::string& numbers) {
     if (numbers.empty()) {
         return 0;
     }
-    
+
     // Set default delimiters
     std::string delimiter = ",\n";
     std::string numbersToProcess = numbers;
-    
+
     // Handle custom delimiters
     if (DelimiterParser::hasCustomDelimiter(numbers)) {
         delimiter = DelimiterParser::extractCustomDelimiter(numbers);
         numbersToProcess = DelimiterParser::extractNumbers(numbers);
     }
-    
+
     // Process numbers
     auto parsedNumbers = NumberProcessor::parseNumbers(numbersToProcess, delimiter);
     NumberProcessor::validateNumbers(parsedNumbers);
